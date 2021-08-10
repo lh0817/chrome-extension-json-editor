44fbf81f0410aad1bcc7401516694f2b13d56f2b
administrator <hannes@mustermann.com>
2021-08-10 11:06:50 +0200

    Add two synchronized editors with different views

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 import JSONEditor from 'jsoneditor'
 
 function init() {
+  // init options for both editors
+  const options = {
+    modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+    onError: function (err) {
+      alert(err.toString())
+    },
+    onModeChange: function (newMode, oldMode) {
+      console.log('Mode switched from', oldMode, 'to', newMode)
+    },
+  }
 
-    // create the editor
-    const container = document.getElementById("jsoneditor")
-    const options = {}
-    const editor = new JSONEditor(container, options)
-  
-    // set json
-    const initialJson = {
-        "Array": [1, 2, 3],
-        "Boolean": true,
-        "Null": null,
-        "Number": 123,
-        "Object": {"a": "b", "c": "d"},
-        "String": "Hello World"
-    }
-    editor.set(initialJson)
-  
-    // get json
-    const updatedJson = editor.get()
+  // create editor 1
+  const editor1 = new JSONEditor(document.getElementById('jsoneditor1'), {
+    ...options,
+    mode: 'text',
+    onChangeText: function (jsonString) {
+      editor2.updateText(jsonString)
+    },
+  })
+
+  // create editor 2
+  const editor2 = new JSONEditor(document.getElementById('jsoneditor2'), {
+    ...options,
+    mode: 'tree',
+    onChangeText: function (jsonString) {
+      editor1.updateText(jsonString)
+    },
+  })
+
+  // set initial data in both editors
+  const json = {
+    'array': [1, 2, 3],
+    'boolean': true,
+    'null': null,
+    'number': 123,
+    'object': {'a': 'b', 'c': 'd'},
+    'string': 'Hello World'
+  }
+  editor1.set(json)
+  editor2.set(json)
 }
 
 // init extension
