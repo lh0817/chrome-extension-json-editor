89dc41cdd087f1f51c94d000057705c34b0f47bb
administrator <hannes@mustermann.com>
2021-08-10 09:06:51 +0200

    Update index.js: jsoneditor Import, basic init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+import JSONEditor from 'jsoneditor'
+
 function init() {
-  console.log('init')
+
+    // create the editor
+    const container = document.getElementById("jsoneditor")
+    const options = {}
+    const editor = new JSONEditor(container, options)
+  
+    // set json
+    const initialJson = {
+        "Array": [1, 2, 3],
+        "Boolean": true,
+        "Null": null,
+        "Number": 123,
+        "Object": {"a": "b", "c": "d"},
+        "String": "Hello World"
+    }
+    editor.set(initialJson)
+  
+    // get json
+    const updatedJson = editor.get()
 }
 
 // init extension
